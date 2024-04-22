f873d7a5dedb1b9a664fd077374d5b2c795304f7
Oleksandr <alex@moonion.com>
2024-04-22 12:30:19 +0200

    version 0.3.0

--- src/renderer/src/pages/Changelog/index.tsx
+++ src/renderer/src/pages/Changelog/index.tsx
@@ -12,46 +12,23 @@ const items = [
   {
     children: (
       <>
-        <Text size="sm">0.3.3 - 18.04.2024</Text>
-        <p>New: #49 Update logo and icons</p>
+        <Text size="sm">0.3.0 beta - 22.04.2024</Text>
         <p>New: #13 Windows signing app</p>
-      </>
-    )
-  },
-  {
-    children: (
-      <>
-        <Text size="sm">0.3.2 - 17.04.2024</Text>
         <p>New: #44 MacOS signing app</p>
+        <p>New: #49 Update logo and icons</p>
         <p>New: #42 Adding a changelog page</p>
-        <p>Improve: #44 reduce app size</p>
-        <p>Fix: Adding additional workers</p>
-        <p>Update: Validator binary e05e4</p>
-        <p>Update: Coordinator binary ee5d6</p>
-      </>
-    )
-  },
-  {
-    children: (
-      <>
-        <Text size="sm">0.3.1 - 15.04.2024</Text>
-        <p>Improve: #10 Adding an auto update feature</p>
-      </>
-    )
-  },
-  {
-    children: (
-      <>
-        <Text size="sm">0.3.0 - 15.04.2024</Text>
         <p>New: #10 Adding an auto update feature</p>
         <p>New: #10 Adding App version to Status Bar</p>
+        <p>Improve: #44 reduce app size</p>
+        <p>Fix: Adding additional workers</p>
       </>
     )
   },
+
   {
     children: (
       <>
-        <Text size="sm">0.2.1 - 10.04.2024</Text>
+        <Text size="sm">0.2.1 beta - 10.04.2024</Text>
         <p>Improve: Save all errors to log file without alert</p>
         <p>Fix: Get Validator status</p>
       </>
@@ -60,7 +37,7 @@ const items = [
   {
     children: (
       <>
-        <Text size="sm">0.2.0 - 05.04.2024</Text>
+        <Text size="sm">0.2.0 beta - 05.04.2024</Text>
         <p>New: Initial Public release</p>
       </>
     )
